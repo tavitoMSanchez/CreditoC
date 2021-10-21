652c81da6a7b21207e34ec5b7e59788dd1b4474e
xxmarroquinxx <marroquins92@hotmail.com>
2021-10-21 09:32:35 -0500

    modificar text meta

--- index.php
+++ index.php
@@ -13,12 +13,12 @@
 
         <title>MaMex</title>
 
-        <meta name="description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="description" content="">
         <meta name="author" content="pixelcave">
         <meta name="robots" content="noindex, nofollow">
-        <meta property="og:title" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework">
+        <meta property="og:title" content="">
         <meta property="og:site_name" content="Codebase">
-        <meta property="og:description" content="Codebase - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta property="og:description" content="">
         <meta property="og:type" content="website">
         <meta property="og:url" content="">
         <meta property="og:image" content="">
